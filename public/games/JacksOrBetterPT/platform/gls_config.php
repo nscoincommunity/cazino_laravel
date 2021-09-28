91cedc1bcfee865027ec29ff2ab779d1770f4868
AlexeySamsonofff <alexeysamsonofff@gmail.com>
2021-09-28 07:00:41 +0300

    add games

---
+++ public/games/JacksOrBetterPT/platform/gls_config.php
@@ -0,0 +1,4 @@
+var gls_config = {
+    "path": "/games/JacksOrBetterPT/platform/location.js",
+    "minutesPerGeoLocationRequest": 240
+}
---
+++ public/games/RouletteClassicPT/platform/gls_config.php
@@ -0,0 +1,4 @@
+var gls_config = {
+    "path": "/games/RouletteClassicPT/platform/location.js",
+    "minutesPerGeoLocationRequest": 240
+}
